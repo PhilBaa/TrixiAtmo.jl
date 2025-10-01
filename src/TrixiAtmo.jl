543321dd1087f8b79fc83be19df3d5ceaf0f9d66
Marco Artiano <57838732+MarcoArtiano@users.noreply.github.com>
2025-10-01 12:27:44 +0200

    Add Potential Temperature formulation (#113)
    * first commit
    
    * format, add tests and some references
    
    * format
    
    * format and typos
    
    * add test
    
    * fix
    
    * fix tests
    
    * format
    
    * format and fix tests
    
    * add more coverage
    
    * fix test values
    
    * format
    
    * fix tests and format
    
    * correct formatting
    
    * other fixes
    
    * add tolerance
    
    * fix
    
    * format and fix tests
    
    * fix test tol
    
    * add rtol
    
    * update tests
    
    * increase coverage
    
    * reduce tol
    
    * change rtol to atol
    
    * increase coverage
    
    * fix tgv
    
    * fix test
    
    * formatT
    
    * format and fix flux lmars
    
    * format and fix flux lmars
    
    * increase coverage and add robert smooth bubble
    
    * add tests for well balancing
    
    * add test for robert bubble
    
    * fix test values
    
    * Update examples/elixir_euler_potential_temperature_baroclinic_instability.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update examples/elixir_euler_potential_temperature_baroclinic_instability.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update examples/elixir_euler_potential_temperature_baroclinic_instability.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update examples/elixir_euler_potential_temperature_baroclinic_instability.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update examples/elixir_euler_potential_temperature_baroclinic_instability.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update examples/elixir_euler_potential_temperature_baroclinic_instability.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * apply review suggestions
    
    * add comments and fix export
    
    * apply review suggestions
    
    * fix Float32 baroclinic instability
    
    * better formatting
    
    * add description of the parameters
    
    * format
    
    * format
    
    * apply suggestions to mountain wave test cases
    
    * fix solve call
    
    * fix mountain schaer
    
    * fix solve calls
    
    * fix solve calls, add ;
    
    * change test values
    
    * fix test values
    
    * fix tests
    
    * Update examples/elixir_euler_potential_temperature_baroclinic_instability.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_euler_potential_temperature_gravity_1d.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_euler_potential_temperature_gravity_2d.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_euler_potential_temperature_gravity_3d.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_euler_potential_temperature_gravity_3d.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_euler_potential_temperature_gravity_3d.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_euler_potential_temperature_gravity_3d.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_euler_potential_temperature_gravity_3d.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_euler_potential_temperature_gravity_2d.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_euler_potential_temperature_gravity_2d.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update src/equations/compressible_euler_potential_temperature_gravity_3d.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * Update examples/elixir_euler_potential_temperature_baroclinic_instability.jl
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>
    
    * add LinearAlgebra to Project.toml in the test env
    
    * restore values in baroclinic instability
    
    * fix typos
    
    * update tests
    
    * format
    
    * fix test values
    
    * format
    
    ---------
    
    Co-authored-by: Hendrik Ranocha <ranocha@users.noreply.github.com>

--- src/TrixiAtmo.jl
+++ src/TrixiAtmo.jl
@@ -24,7 +24,12 @@ using HDF5: HDF5, h5open, attributes, create_dataset, datatype, dataspace
 using Setfield
 
 @reexport using StaticArrays: SVector, SMatrix
-@reexport import Trixi: waterheight
+@reexport import Trixi: waterheight, varnames, cons2cons, cons2prim, prim2cons,
+                        cons2entropy, boundary_condition_slip_wall, flux, flux_ec,
+                        flux_chandrashekar, FluxLMARS, energy_total,
+                        max_abs_speeds, max_abs_speed_naive, max_abs_speed, energy_kinetic,
+                        energy_total, entropy, pressure, have_nonconservative_terms
+using Trixi: ln_mean, stolarsky_mean, inv_ln_mean
 
 # DGMulti solvers
 using StartUpDG: RefElemData, MeshData, AbstractElemShape
@@ -38,7 +43,13 @@ include("callbacks_step/callbacks_step.jl")
 
 export CompressibleMoistEulerEquations2D, ShallowWaterEquations3D,
        CovariantLinearAdvectionEquation2D, CovariantShallowWaterEquations2D,
-       SplitCovariantShallowWaterEquations2D
+       SplitCovariantShallowWaterEquations2D,
+       CompressibleEulerPotentialTemperatureEquations1D,
+       CompressibleEulerPotentialTemperatureEquations2D,
+       CompressibleEulerPotentialTemperatureEquations3D,
+       CompressibleEulerPotentialTemperatureEquationsWithGravity1D,
+       CompressibleEulerPotentialTemperatureEquationsWithGravity2D,
+       CompressibleEulerPotentialTemperatureEquationsWithGravity3D
 
 export GlobalCartesianCoordinates, GlobalSphericalCoordinates
 
@@ -46,7 +57,10 @@ export flux_chandrashekar, FluxLMARS
 
 export flux_nonconservative_zeros, flux_nonconservative_ec,
        flux_nonconservative_surface_simplified, source_terms_geometric_coriolis,
-       source_terms_coriolis, source_terms_coriolis_lagrange_multiplier
+       source_terms_coriolis, source_terms_coriolis_lagrange_multiplier,
+       flux_tec, flux_etec, flux_nonconservative_souza_etal,
+       flux_nonconservative_artiano_etal,
+       flux_nonconservative_waruzewski_etal, flux_zero
 
 export source_terms_lagrange_multiplier, clean_solution_lagrange_multiplier!
 
@@ -70,4 +84,5 @@ export initial_condition_gaussian, initial_condition_geostrophic_balance,
 export bottom_topography_isolated_mountain, bottom_topography_unsteady_solid_body_rotation
 
 export examples_dir
+
 end # module TrixiAtmo
